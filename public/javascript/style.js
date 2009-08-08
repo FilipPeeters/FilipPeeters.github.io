1a92b2cbd5b5f17147bd396a9c2c4a687d87926a
Johan Peeters <yo@johanpeeters.com>
2009-08-08 22:16:41 +0200

    added the type attribute to the embed tag in movieviewer

--- public/javascript/style.js
+++ public/javascript/style.js
@@ -1,25 +1,3 @@
-function setActiveStyleSheet(title) {
-	var i, a;
-	for(i=0; (a = document.getElementsByTagName("link")[i]); i++) {
-	if(a.getAttribute("rel").indexOf("style") != -1
-		&& a.getAttribute("title")) {
-		a.disabled = true;
-		if(a.getAttribute("title") == title) a.disabled = false;
-		}	
-	}
-	setVisible('img', false);
-	setVisible('div', false);
-}
-function setVisible(tagName, b) {
-	var i, a;
-	for(i=0; a = document.getElementsByTagName(tagName)[i]; i++) {
-		if (b){
-			a.style.visibility = 'visible';
-		} else {
-			a.style.visibility = 'hidden';
-		}
-	}
-}
 function setVisibleById(id, b) {
 	var a;
 	a = document.getElementById(id);
@@ -28,9 +6,4 @@ function setVisibleById(id, b) {
 	} else {
 		a.style.display = 'none';
 	}
-}
-
-function openFilmWindow(movieToPlay){
-		document.getElementById('film').value = movieToPlay;
-		window.open('movieviewer.html','player', 'target=_blank');
 }
